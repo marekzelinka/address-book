e78564a52ff54d8292161e2b5e551d51c2356fed
Marek Zelinka <mzelinka17@gmail.com>
2024-12-12 20:58:58 +0100

    add about page

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { index, route, type RouteConfig } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("contacts/:contactId", "routes/contact.tsx"),
+  route("about", "routes/about.tsx"),
 ] satisfies RouteConfig;
